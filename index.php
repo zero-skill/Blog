77788493b5e434226bb9daa2e63f590ab0942cbd
zeroSkill <gabbriel.amaya@gmail.com>
2021-03-24 19:13:22 -0300

    Header

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <title>Blog de videojuegos</title>
+    </head>
+    <body>
+   <!---- HEADER --->
+   <header>
+       <div id="logo">
+           <a href="index.php">Blog de videojuegos</a>
+       </div>
+   </header>
+   <!---- MENU --->
+   <!---- BARRA LATERAL ---> 
+   <!---- CAJA PRINCIPAL --->
+   <!---- FOOTER --->
+    </body>
+</html>
